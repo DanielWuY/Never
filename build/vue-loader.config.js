803c0e033455b145f8d37b94e8d261ffc87e74c1
Daniel <wuyongdaniel@163.com>
2018-04-30 22:00:10 +0800

    modify build config

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -2,6 +2,9 @@ module.exports = (isDev) => {
     return {
         preserveWhitespace: true,
         extractCSS: !isDev,
-        cssModules: {}
+        cssModules: {
+            localIdentName: isDev ? '[path][name]-[hash:base64:5]' : '[hash:base64:5]',
+            camelCase: true
+        }
     }
 }
