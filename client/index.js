298235e4b2cfe7113a5774c6b894c203ed4252cf
Daniel <wuyongdaniel@163.com>
2018-04-30 23:24:07 +0800

    add template.html

--- client/index.js
+++ client/index.js
@@ -3,9 +3,6 @@ import App from './App.vue';
 
 import './assets/styles/global.styl';
 
-const root = document.createElement('div');
-document.body.appendChild(root);
-
 new Vue({
     render: (h) => h(App)
-}).$mount(root);
+}).$mount('#root');
